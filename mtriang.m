bf0fd667a4fc1559b80942765671f15354deaf4f
Emilio Jorge <emiliojorge>
2015-06-02 11:17:28 +0200

    dåligt försök mtriang

--- mtriang.m
+++ mtriang.m
@@ -1,5 +1,5 @@
 function mtriang
-n = 4000;
+n = 400;
 A = triu(randn(n));
 B = triu(randn(n));
 
@@ -8,8 +8,22 @@ tic;
 toc
 
 tic
-  % Your code here...
-  C = ...
+  C=zeros(n);
+   %utilizing columnfirst.
+  for j=1:n %columnfirst
+    for i=1:j
+    C(i,j)=A(i,i:j)*B(i:j,j);
+    end
+  end
+toc
+C;
+tic
+  D=zeros(4000);
+  A=A';
+  for j=1:4000
+    for i=1:4000
+    end
+end   
 toc
 
 error = norm(F - C, 1)
